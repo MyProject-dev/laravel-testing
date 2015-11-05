b3e19cac83f264ae26c74dec5ac14b6911d70760
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2015-11-05 15:42:10 -0800

    restful resource controllers with routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,7 +9,46 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+/**
+ * SRC: http://laravel.com/docs/5.1/controllers#restful-resource-controllers
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+/**
+ * RESTful Resource Controllers
+ * create controller using command line: php artisan make:controller PhotoController
+ * route: Route::resource('photo', 'PhotoController');
+ * Actions Handled By Resource Controller
+ *
+ * Partial Resource Routes
+ * Route::resource('photo', 'PhotoController',
+ * ['only' => ['index', 'show']]);
+ * Route::resource('photo', 'PhotoController',
+ * ['except' => ['create', 'store', 'update', 'destroy']]);
+ *
+ *
+ *
+ */
+
+//index
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//photo
+Route::resource('photo', 'PhotoController',
+    ['only' => ['index', 'show']]);
+
+//photo comments
+//Nested Resources
+Route::resource('photo.comments', 'PhotoCommentController',
+    ['except' => ['create', 'store', 'update', 'destroy', 'edit']]);
+
+//photo comments like
+Route::resource('photo.comment.likes', 'PhotoCommentLikeController',
+    ['only' => ['index', 'show', 'create']]);
+
+
+//Supplementing Resource Controllers
+//Route::get('photos/popular', 'PhotoController@method');
+//Route::resource('photos', 'PhotoController');
